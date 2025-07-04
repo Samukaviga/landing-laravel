1c387bf351891dbdda5620abe48e90a39c0c7126
samuel <samuelgomes101112@gmail.com>
2025-07-04 10:01:04 -0300

    add logo liceu da beleza

--- resources/views/components/navbar-index.blade.php
+++ resources/views/components/navbar-index.blade.php
@@ -1,11 +1,13 @@
 <!-- STRAT NAVBAR -->
 <nav id="navbar" class="navbar navbar-expand-lg fixed-top sticky">
-    <a class="navbar-brand" href="/" style="padding: 0 50px">
-        <img src="assets/images/logo-dark.png" alt="logo" height="50" class="logo-light" />
+    <a class="navbar-brand nav-link" href="/" style="padding: 0 28px">
+        <img src="assets/images/logo-dark.png" alt="logo" height="40" class="logo-light" />
+        <img src="assets/images/logo-liceu-da-beleza-branco.png" alt="logo" height="40" class="logo-light" />
     </a>
     <div class="div-mobile-logo">
-        <a class="navbar-brand" href="/">
-            <img src="assets/images/logo-dark.png" alt="logo" height="40" class="logo-dark" />
+        <a class="navbar-brand nav-link-mobile" href="/">
+            <img src="assets/images/logo-dark.png" alt="logo" height="30" class="logo-dark" />
+            <img src="assets/images/logo-liceu-da-beleza-branco.png" alt="logo" height="30" class="logo-dark" />
         </a>
     </div>
     <div class="container">
